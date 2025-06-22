56bad4b66b4321bbc5c6f6e2d0c3767016a17d31
Faisal Karim <faisalchy64@gmail.com>
2025-06-22 22:05:44 +0600

    (feat): Updated server.ts file.

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,11 @@ const PORT = 5000;
 
 async function main(): Promise<void> {
   try {
+    app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
     await mongoose.connect(
       "mongodb+srv://faisalchy64:1o45Le48IrGB7bfE@library-management-syst.xnoa4rr.mongodb.net/library?retryWrites=true&w=majority&appName=Library-Management-System"
     );
     console.log("Database connected successfully...");
-    app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
   } catch (error) {
     console.log("Database connection failed");
   }
